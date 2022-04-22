51ce69a0065f98e17503836004ddc73b5d539fc7
Jose Cancino <jluiscancino115@gmail.com>
2022-04-21 20:58:45 -0500

    agregando nueva ruta

--- app.js
+++ app.js
@@ -14,3 +14,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
+
+// Respondiendo texto
+// localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+/* // Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer =  {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/Jose */
